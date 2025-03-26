7b1744bed78a5e9f9a44621df84b47ecb0126117
Rafaela Souza <rssrafaelaa@gmail.com>
2025-03-25 23:23:04 -0300

    Adicionando as aulas 1 a 4 com alterações e aula 11 que aprendi na Infnet.

--- Aula-03.py
+++
@@ -1,7 +0,0 @@
-min = [120]
-hrs = min 60
-seg = min * 60
-dia = (min/60) / 24
-texto = str(main) + "minutos equivalem a " + str(dia) +" dias, " + str(hrs) + "horas e" + str(seg) + "segundos."
-
-print(texto)  
